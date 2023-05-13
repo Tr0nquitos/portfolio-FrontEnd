2cec831c394e27c4393a725c22244407053c9722
Tr0nquitos <mateotroncoso154@gmail.com>
2023-05-13 00:03:30 -0300

    Prueba4

--- src/app/components/exp-laboral/exp-laboral.component.ts
+++ src/app/components/exp-laboral/exp-laboral.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ExpLaboral } from 'src/app/model/exp-laboral';
+import { ExpServiceService } from 'src/app/service/exp-service.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-exp-laboral',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./exp-laboral.component.css']
 })
 export class ExpLaboralComponent {
+  experiencia: ExpLaboral[] = [];
 
+  constructor(private ExpService:ExpServiceService, private tokenService: TokenService){}
+
+  isLogged = false;
+
+  ngOnInit(): void {
+    this.cargarExperiencia();
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    } else {
+      this.isLogged = false;
+    }
+
+  }
+
+  cargarExperiencia():void{
+    this.ExpService.lista().subscribe(
+      data => this.experiencia = data
+    )
+
+  }
 }
